957a4e7a832e96d9202cfc5559f9654e6170f630
tan <20920380@qq.com>
2019-10-23 16:58:21 +0800

    修改了上传路径

--- src/main/java/com/example/csdj/member/controller/IndexController.java
+++ src/main/java/com/example/csdj/member/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.example.csdj.common.web.BaseController;
 import com.example.csdj.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +14,8 @@ public class IndexController extends BaseController {
     MemberService memberService;
 
     @RequestMapping("/")
-    public String index() {
-        memberService.addPartyMember();
-        return "member/index.html";
+    public String index(Model model) {
+        return "abc";
     }
 
     @RequestMapping("/index")
