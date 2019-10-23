04b9f84a286d3cd73bab0f80a35acc6f12c97a21
wangxingxing111 <2410907095@qq.com>
2019-10-23 17:31:43 +0800

    王星提交

---
+++ src/main/java/com/example/csdj/branch/controller/MoneyManagementController.java
@@ -0,0 +1,16 @@
+package com.example.csdj.branch.controller;
+
+import com.example.csdj.common.web.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/*
+* 党费管理
+* */
+@RequestMapping("/branch")
+@Controller
+public class MoneyManagementController extends BaseController {
+    @RequestMapping("/MoneyManagement")
+    public String MoneyManagement(){
+        return "/branch/money/moneyList";
+    }
+}
--- src/main/java/com/example/csdj/branch/controller/StatisticalAnalysisController.java
+++ src/main/java/com/example/csdj/branch/controller/StatisticalAnalysisController.java
@@ -1,15 +1,12 @@
 package com.example.csdj.branch.controller;
 
+import com.example.csdj.common.web.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
+/*
+* 统计分析
+* */
 @RequestMapping("/branch")
-public class Test {
-    @RequestMapping("/index")
-    public String index(){
-
-        return "/branch/index";
-    }
-
+@Controller
+public class StatisticalAnalysisController extends BaseController {
 }
