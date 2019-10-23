f9c2af73a57add70584a01e1c0d5b30a80c55137
guo7598 <ghr18347476889@163.com>
2019-10-23 20:57:39 +0800

    郭浩冉党费单个提交

--- src/main/java/com/example/csdj/branch/controller/MoneyManagementController.java
+++ src/main/java/com/example/csdj/branch/controller/MoneyManagementController.java
@@ -4,13 +4,17 @@ import com.example.csdj.common.web.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /*
-* 党费管理
-* */
+ * 党费管理
+ * */
 @RequestMapping("/branch")
 @Controller
 public class MoneyManagementController extends BaseController {
     @RequestMapping("/MoneyManagement")
     public String MoneyManagement(){
         return "/branch/money/moneyList";
+    }
+    @RequestMapping("/moneyAfter")
+    public String moneyAfter(){
+        return "/branch/money/moneyAfter";
     }
 }
