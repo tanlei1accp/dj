7e9ac1a3c1afcf9d5d0f40944241b9eb67e568cf
houcheng <2238768684@qq.com>
2019-10-24 09:50:28 +0800

    更改路径

--- src/main/java/com/example/csdj/committee/controller/CommitteeController.java
+++ src/main/java/com/example/csdj/committee/controller/CommitteeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("committee")
 @Controller
-public class IndexController1 {
+public class CommitteeController {
     @RequestMapping("/index")
     public String showIndex(){
         return "committee/index";
