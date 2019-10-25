5c937321bf917c634bd7d21f1a1cf7df26ae9460
guo7598 <ghr18347476889@163.com>
2019-10-25 10:49:57 +0800

    郭浩冉实体类提交

--- src/main/java/com/example/csdj/branch/entity/Activity.java
+++ src/main/java/com/example/csdj/branch/entity/Activity.java
@@ -7,5 +7,88 @@ public class Activity {
     private String activityPeople;
     private String activityRecord;
     private int activityFlag;
-    private int departmentId;
+    private Department departmentId;
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityHoldingTime() {
+        return activityHoldingTime;
+    }
+
+    public void setActivityHoldingTime(String activityHoldingTime) {
+        this.activityHoldingTime = activityHoldingTime;
+    }
+
+    public String getActivityPlace() {
+        return activityPlace;
+    }
+
+    public void setActivityPlace(String activityPlace) {
+        this.activityPlace = activityPlace;
+    }
+
+    public String getActivityPeople() {
+        return activityPeople;
+    }
+
+    public void setActivityPeople(String activityPeople) {
+        this.activityPeople = activityPeople;
+    }
+
+    public String getActivityRecord() {
+        return activityRecord;
+    }
+
+    public void setActivityRecord(String activityRecord) {
+        this.activityRecord = activityRecord;
+    }
+
+    public int getActivityFlag() {
+        return activityFlag;
+    }
+
+    public void setActivityFlag(int activityFlag) {
+        this.activityFlag = activityFlag;
+    }
+
+    public Department getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Department departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "activityId=" + activityId +
+                ", activityHoldingTime='" + activityHoldingTime + '\'' +
+                ", activityPlace='" + activityPlace + '\'' +
+                ", activityPeople='" + activityPeople + '\'' +
+                ", activityRecord='" + activityRecord + '\'' +
+                ", activityFlag=" + activityFlag +
+                ", departmentId=" + departmentId +
+                '}';
+    }
+
+    public Activity(int activityId, String activityHoldingTime, String activityPlace, String activityPeople, String activityRecord, int activityFlag, Department departmentId) {
+        this.activityId = activityId;
+        this.activityHoldingTime = activityHoldingTime;
+        this.activityPlace = activityPlace;
+        this.activityPeople = activityPeople;
+        this.activityRecord = activityRecord;
+        this.activityFlag = activityFlag;
+        this.departmentId = departmentId;
+    }
+
+    public Activity() {
+        super();
+    }
 }
--- src/main/java/com/example/csdj/branch/entity/Money.java
+++ src/main/java/com/example/csdj/branch/entity/Money.java
@@ -6,6 +6,59 @@ import java.math.BigDecimal;
 public class Money {
     private int moneyId;
     private int moneyMonth;
-    private int memberId;
+    private Member memberId;
     private BigDecimal moneyNumber;
+
+    public int getMoneyId() {
+        return moneyId;
+    }
+
+    public void setMoneyId(int moneyId) {
+        this.moneyId = moneyId;
+    }
+
+    public int getMoneyMonth() {
+        return moneyMonth;
+    }
+
+    public void setMoneyMonth(int moneyMonth) {
+        this.moneyMonth = moneyMonth;
+    }
+
+    public Member getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Member memberId) {
+        this.memberId = memberId;
+    }
+
+    public BigDecimal getMoneyNumber() {
+        return moneyNumber;
+    }
+
+    public void setMoneyNumber(BigDecimal moneyNumber) {
+        this.moneyNumber = moneyNumber;
+    }
+
+    public Money(int moneyId, int moneyMonth, Member memberId, BigDecimal moneyNumber) {
+        this.moneyId = moneyId;
+        this.moneyMonth = moneyMonth;
+        this.memberId = memberId;
+        this.moneyNumber = moneyNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "moneyId=" + moneyId +
+                ", moneyMonth=" + moneyMonth +
+                ", memberId=" + memberId +
+                ", moneyNumber=" + moneyNumber +
+                '}';
+    }
+
+    public Money() {
+        super();
+    }
 }
