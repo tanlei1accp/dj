881f98ec4f0fee9e9c555d064f54d4ba68e0735b
guo7598 <ghr18347476889@163.com>
2019-10-25 10:34:48 +0800

    Merge branch 'master' of C:\Users\Administrator\Desktop\djt with conflicts.

---
+++ src/main/java/com/example/csdj/branch/entity/Activity.java
@@ -0,0 +1,11 @@
+package com.example.csdj.branch.entity;
+
+public class Activity {
+    private int activityId;
+    private String activityHoldingTime;
+    private String activityPlace;
+    private String activityPeople;
+    private String activityRecord;
+    private int activityFlag;
+    private int departmentId;
+}
---
+++ src/main/java/com/example/csdj/branch/entity/Money.java
@@ -0,0 +1,11 @@
+package com.example.csdj.branch.entity;
+
+
+import java.math.BigDecimal;
+
+public class Money {
+    private int moneyId;
+    private int moneyMonth;
+    private int memberId;
+    private BigDecimal moneyNumber;
+}
