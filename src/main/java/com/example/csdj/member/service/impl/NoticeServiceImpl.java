4f9749b5a625e9f9c9ad1819690cbdb9b57d0b3d
hmy666 <726415801@qq.com>
2019-10-25 18:49:12 +0800

    郝明宇第三次提交

--- src/main/java/com/example/csdj/member/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/example/csdj/member/service/impl/NoticeServiceImpl.java
@@ -1,14 +1,17 @@
 package com.example.csdj.member.service.impl;
 
+import com.example.csdj.common.service.impl.BaseServiceImpl;
 import com.example.csdj.member.entity.Notice;
 import com.example.csdj.member.mapper.NoticeMapper;
 import com.example.csdj.member.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
+@Service
+@Transactional(rollbackFor = Exception.class)
 public class NoticeServiceImpl implements NoticeService {
 
     @Autowired
@@ -16,26 +19,26 @@ public class NoticeServiceImpl implements NoticeService {
 
     @Override
     public List<Notice> findAll() {
-        return null;
+        return noticeMapper.findAll();
     }
 
     @Override
     public void addNotice(Notice notice) {
-
+        noticeMapper.addNotice(notice);
     }
 
     @Override
     public Notice findById(int id) {
-        return null;
+        return noticeMapper.findById(id);
     }
 
     @Override
     public void delById(Notice notice) {
-
+        noticeMapper.delById(notice);
     }
 
     @Override
     public void updById(Notice notice) {
-
+        noticeMapper.updById(notice);
     }
 }
